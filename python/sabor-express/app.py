b03f4106d51315b7e580416e166c10fe0b976b02
Marcelo Soares Moura <marcelosmoura@hotmail.com>
2024-05-02 14:03:28 -0300

    Atualização do arquivo app.js

---
+++ python/sabor-express/app.py
@@ -0,0 +1,36 @@
+print('''
+░██████╗░█████╗░██████╗░░█████╗░██████╗░███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝█████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░ 
+      ''')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção { opcao_escolhida}')
+
+# print('Python na Escola de Programação da Alura')
+
+# nome = 'Marcelo'
+# idade = 51
+
+# print(f'Meu nome é {nome} e tenho {idade} anos.')
+
+# print ('A','L','U','R','A',sep='\n')
+
+# pi = 3.14159
+
+# Abordagem de f-string
+# print(f'O valor arredondado de pi é: {pi:.2f}')
+
+# Abordagem de .format()
+# print('O valor arredondado de pi é: {:.2f}'.format(pi))
+
+# Utilizando a função round()
+# print('O valor arredondado de pi é:', round(pi, 2))
