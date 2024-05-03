98efe9ff0489497c4a816657d82083f3176cd56e
Marcelo Soares Moura <marcelosmoura@hotmail.com>
2024-05-03 10:46:34 -0300

    #	new file:   python/exercicios - manipulacao de strings.txt #	modified:   python/sabor-express/app.py

--- python/sabor-express/app.py
+++ python/sabor-express/app.py
@@ -13,24 +13,12 @@ print('3. Ativar restaurante')
 print('4. Sair\n')
 
 opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção { opcao_escolhida}')
 
-# print('Python na Escola de Programação da Alura')
-
-# nome = 'Marcelo'
-# idade = 51
-
-# print(f'Meu nome é {nome} e tenho {idade} anos.')
-
-# print ('A','L','U','R','A',sep='\n')
-
-# pi = 3.14159
-
-# Abordagem de f-string
-# print(f'O valor arredondado de pi é: {pi:.2f}')
-
-# Abordagem de .format()
-# print('O valor arredondado de pi é: {:.2f}'.format(pi))
-
-# Utilizando a função round()
-# print('O valor arredondado de pi é:', round(pi, 2))
+if opcao_escolhida == 1:
+    print("Cadastrar restaurante")
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurente')
+else:
+    print('Finalizar o programa')
